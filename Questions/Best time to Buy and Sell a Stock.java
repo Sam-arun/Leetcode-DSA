b2e6c304e964ede9dba6a2a55f63ed1b7f492a17
Arun S <74012433+Sam-arun@users.noreply.github.com>
2022-10-23 18:57:47 +0530

    Add files via upload

---
+++ Questions/Best time to Buy and Sell a Stock.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        int maxPro = maxProfit(arr);
+        System.out.println("Max profit is: " + maxPro);
+    }
+
+    static int maxProfit(int[] arr) {
+        int maxPro = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    maxPro = Math.max(arr[j] - arr[i], maxPro);
+                }
+            }
+        }
+        return maxPro;
+    }
+}
